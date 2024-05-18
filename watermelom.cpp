77852a1bda6eee742513b266b5ad7ab6697e982f
Maynul Islam Munna <133635068+Maynul-Islam@users.noreply.github.com>
2024-05-18 13:19:04 +0600

    Codeforces Beta Round 4 (Div. 2 Only) problem A

---
+++ watermelom.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int w;
+
+    cin>>w;
+
+    if(w>2)
+    {
+        if (w % 2 == 0)
+    {
+        cout << "YES"<<endl;
+    }
+    else
+    {
+        cout <<"NO"<<endl;
+    }
+    }
+
+    else
+    {
+        cout <<"NO"<<endl;
+    }
+    return 0;
+
+}
