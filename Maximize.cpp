aedac918ddf85ce8ea7196b62bd24ac29edd666b
Maynul Islam Munna <133635068+Maynul-Islam@users.noreply.github.com>
2024-05-19 22:48:26 +0600

    Codeforces problem Maximize solution

---
+++ Maximize.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(void)
+{
+    int x;
+    cin>>x;
+    cout << x-1 <<endl;
+    return;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
