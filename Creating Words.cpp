2221a84bc9c83a365b59730e715094f857983700
Maynul Islam Munna <133635068+Maynul-Islam@users.noreply.github.com>
2024-06-11 22:47:54 +0600

    Codeforces problem creating file solution

---
+++ Creating Words.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve()
+{
+    string a,b;
+    cin>>a>>b;
+    cout<<b[0]<<a[1]<<a[2]<<" "<<a[0]<<b[1]<<b[2]<<"\n";
+    return;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;N
+}
