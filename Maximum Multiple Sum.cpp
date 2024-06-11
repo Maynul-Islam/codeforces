9bcaf27240621543949fd654b569e9a38dc6054f
Maynul Islam Munna <133635068+Maynul-Islam@users.noreply.github.com>
2024-06-11 22:48:46 +0600

    Codeforces problem Maximum multiple sum solution

---
+++ Maximum Multiple Sum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void solve()
+{
+    int n;
+    cin>>n;
+    if(n==3)
+        cout << "3\n";
+    else
+        cout <<"2\n";
+
+
+    return;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
